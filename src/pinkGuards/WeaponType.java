05825ada3e6abe91182edb0cb51727ce94e809ff
somvras <mikaelbryyovskyy@gmail.com>
2025-02-20 19:18:44 +0100

    Update WeaponType.java

--- src/pinkGuards/WeaponType.java
+++ src/pinkGuards/WeaponType.java
@@ -1,7 +1,17 @@
 package pinkGuards;
 
 public enum WeaponType {
-    GLOCK17,
-    MP5,
-    G3
+    GLOCK_17("Pistola Glock 17"),
+    HECKLER_MP5("Rifle de asalto Heckler & Koch MP5"),
+    HECKLER_G3("Rifle automático Heckler & Koch G3");
+
+    private String descripcion;
+
+    WeaponType(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
