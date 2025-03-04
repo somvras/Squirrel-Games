a69e99757b93d74bdb1f5c922fbb1c3a6195aca2
somvras <mikaelbryyovskyy@gmail.com>
2025-03-04 20:54:57 +0100

    Añadida documentación en Rango.java (Mikael)

--- src/pinkGuards/Rango.java
+++ src/pinkGuards/Rango.java
@@ -1,7 +1,18 @@
 package pinkGuards;
 
+/**
+ * Enumerado que define los rangos jerárquicos de los Pink Guards.
+ * <p>
+ * Los rangos disponibles son WORKER, SOLDIER y MANAGER, en orden ascendente de autoridad.
+ * </p>
+ * 
+ * @author Mikael
+ * @version 1.0
+ * @see PinkGuard
+ */
 public enum Rango {
     WORKER,
     SOLDIER,
     MANAGER
 }
+
