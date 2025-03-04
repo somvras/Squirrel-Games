ae7db8992e586aa7de2f8dafc5e5a5fbc7675703
somvras <mikaelbryyovskyy@gmail.com>
2025-03-04 20:54:15 +0100

    Añadida documentación en PinkGuard.java (Mikael)

--- src/pinkGuards/PinkGuard.java
+++ src/pinkGuards/PinkGuard.java
@@ -1,28 +1,72 @@
 package pinkGuards;
+
 import Excepciones.SupervisorNoValidoExcepcion;
 
+/**
+ * Clase abstracta que representa un PinkGuard.
+ * <p>
+ * Los Pink Guards son el personal encargado de tareas en los juegos, y esta clase define atributos y comportamientos comunes a todas sus subclases.
+ * </p>
+ * 
+ * @author Mikael
+ * @version 1.0
+ * @see Worker
+ * @see Soldier
+ * @see Manager
+ */
 public abstract class PinkGuard {
-	protected int id;
+    protected int id;
     protected String nombre;
     protected PinkGuard supervisor;
     
+    /**
+     * Crea una instancia de PinkGuard con un identificador y nombre.
+     * 
+     * @param id El identificador único del PinkGuard.
+     * @param nombre El nombre del PinkGuard.
+     */
     public PinkGuard(int id, String nombre) {
         this.id = id;
         this.nombre = nombre;
     }
     
-    // Cada subclase define su rango
+    /**
+     * Devuelve el rango del PinkGuard.
+     * <p>
+     * Este método debe ser implementado por cada subclase.
+     * </p>
+     * 
+     * @return El rango del PinkGuard.
+     */
     public abstract Rango getRango();
     
+    /**
+     * Obtiene el supervisor del PinkGuard.
+     * 
+     * @return El supervisor asignado o null si no se ha asignado ninguno.
+     */
     public PinkGuard getSupervisor() {
         return supervisor;
     }
     
+    /**
+     * Devuelve el nombre del PinkGuard.
+     * <p>
+     * Este método debe ser implementado por las subclases para retornar correctamente el nombre.
+     * </p>
+     * 
+     * @return El nombre del PinkGuard.
+     */
     public abstract String getNombre();
 
-	/**
+    /**
      * Asigna un supervisor validando que tenga un rango superior.
+     * <p>
      * Si el supervisor es nulo, se asigna sin comprobación.
+     * </p>
+     * 
+     * @param supervisor El PinkGuard a asignar como supervisor.
+     * @throws SupervisorNoValidoExcepcion Si el supervisor no tiene un rango superior.
      */
     public void setSupervisor(PinkGuard supervisor) throws SupervisorNoValidoExcepcion {
         if (supervisor != null) {
@@ -33,6 +77,11 @@ public abstract class PinkGuard {
         this.supervisor = supervisor;
     }
     
+    /**
+     * Representa el objeto en forma de cadena.
+     * 
+     * @return Una cadena que contiene el ID, nombre y rango del PinkGuard, y la información del supervisor si existe.
+     */
     @Override
     public String toString() {
         String info = "ID: " + id + ", Nombre: " + nombre + ", Rango: " + getRango();
@@ -41,6 +90,6 @@ public abstract class PinkGuard {
         }
         return info;
     }
-	
 }
+
 
